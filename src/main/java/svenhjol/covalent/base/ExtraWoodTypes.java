e0d7f78110521220ceec5d56bb94178d77606042
Sven H <sven.hjol@protonmail.com>
2020-05-23 17:40:29 +0000

    Adding wood types WIP

--- src/main/java/svenhjol/covalent/base/ExtraWoodTypes.java
+++ src/main/java/svenhjol/covalent/base/ExtraWoodTypes.java
@@ -3,6 +3,11 @@ package svenhjol.covalent.base;
 import svenhjol.meson.enums.IWoodType;
 
 public enum ExtraWoodTypes implements IWoodType {
+    BAMBOO,
+    DRIFTWOOD,
+    HIVE,
+    MAPLE,
     POISE,
-    DRIFTWOOD
+    ROSEWOOD,
+    WILLOW
 }
---
+++ src/main/java/svenhjol/covalent/decoration/module/BuzzierBees.java
@@ -0,0 +1,39 @@
+package svenhjol.covalent.decoration.module;
+
+import svenhjol.charm.base.CharmCategories;
+import svenhjol.charm.decoration.block.BookshelfChestBlock;
+import svenhjol.charm.decoration.block.CrateOpenBlock;
+import svenhjol.charm.decoration.block.CrateSealedBlock;
+import svenhjol.charm.decoration.block.CustomBarrelBlock;
+import svenhjol.covalent.Covalent;
+import svenhjol.covalent.base.ExtraWoodTypes;
+import svenhjol.meson.MesonModule;
+import svenhjol.meson.enums.IWoodType;
+import svenhjol.meson.iface.Module;
+
+@Module(mod = Covalent.MOD_ID, category = CharmCategories.DECORATION,
+    description = "Adds barrels and crates for Buzzier Bees.")
+public class BuzzierBees extends MesonModule {
+    public static CustomBarrelBlock barrelBlock;
+    public static CrateOpenBlock crateOpenBlock;
+    public static CrateSealedBlock crateSealedBlock;
+    public static BookshelfChestBlock bookshelfChestBlock;
+
+    @Override
+    public void init() {
+        super.init();
+        IWoodType wood = ExtraWoodTypes.HIVE;
+
+        barrelBlock = new CustomBarrelBlock(this, wood);
+        crateOpenBlock = new CrateOpenBlock(this, wood);
+        crateSealedBlock = new CrateSealedBlock(this, wood);
+        bookshelfChestBlock = new BookshelfChestBlock(this, wood);
+    }
+
+    @Override
+    public boolean shouldRunSetup() {
+        // TODO disabled for now because of runtime issues.
+        // return ForgeHelper.isModLoaded("upgrade_aquatic");
+        return true;
+    }
+}

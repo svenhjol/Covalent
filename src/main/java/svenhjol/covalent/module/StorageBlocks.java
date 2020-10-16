5fd541e38e968368400ba360f4abf42b3495ff16
Coranthes <josa23oreo@gmail.com>
2020-10-16 14:36:51 -0400

    Potato, carrot and beetroot textures. Deletion of cyan dye and tulip files.

--- src/main/java/svenhjol/covalent/module/StorageBlocks.java
+++ src/main/java/svenhjol/covalent/module/StorageBlocks.java
@@ -18,10 +18,8 @@ public class StorageBlocks extends MesonModule {
     public static RottenFleshBlock ROTTEN_FLESH_BLOCK;
     public static BambooBlock BAMBOO_BLOCK;
     public static EggBlock EGG_BLOCK;
-    public static RedTulipBlock RED_TULIP_BLOCK;
     public static BeetrootBlock BEETROOT_BLOCK;
     public static PotatoBlock POTATO_BLOCK;
-    public static CyanDyeBlock CYAN_DYE_BLOCK;
     public static CarrotBlock CARROT_BLOCK;
 
     public static List<MesonBlock> STORAGE_BLOCKS = new ArrayList<>();
@@ -47,34 +45,24 @@ public class StorageBlocks extends MesonModule {
     @Config(name = "Egg crate")
     public static boolean egg = true;
 
-    @Config(name = "Jar of Cyan Dye")
-    public static boolean cyanDye = true;
-
-    @Config(name = "Red Tulip Bundle")
-    public static boolean redTulip = true;
-
     @Override
     public void register() {
         WHEAT_SEED_BLOCK = new WheatSeedBlock(this);
         ROTTEN_FLESH_BLOCK = new RottenFleshBlock(this);
         BAMBOO_BLOCK = new BambooBlock(this);
         EGG_BLOCK = new EggBlock(this);
-        RED_TULIP_BLOCK = new RedTulipBlock(this);
         POTATO_BLOCK = new PotatoBlock(this);
         BEETROOT_BLOCK = new BeetrootBlock(this);
         CARROT_BLOCK = new CarrotBlock(this);
-        CYAN_DYE_BLOCK = new CyanDyeBlock(this);
 
         STORAGE_BLOCKS.addAll(Arrays.asList(
             WHEAT_SEED_BLOCK,
             ROTTEN_FLESH_BLOCK,
-            CYAN_DYE_BLOCK,
             BAMBOO_BLOCK,
             EGG_BLOCK,
             BEETROOT_BLOCK,
             POTATO_BLOCK,
-            CARROT_BLOCK,
-            RED_TULIP_BLOCK
+            CARROT_BLOCK
 
         ));
     }
@@ -102,11 +90,6 @@ public class StorageBlocks extends MesonModule {
             recipes.add(new Identifier(Covalent.MOD_ID, "storage_blocks/eggs_from_egg_block"));
         }
 
-        if (!redTulip) {
-            recipes.add(new Identifier(Covalent.MOD_ID, "storage_blocks/red_tulip_block_from_red_tulip"));
-            recipes.add(new Identifier(Covalent.MOD_ID, "storage_blocks/red_tulip_from_red_tulip_block"));
-        }
-
         if (!potato) {
             recipes.add(new Identifier(Covalent.MOD_ID, "storage_blocks/potato_block_from_potato"));
             recipes.add(new Identifier(Covalent.MOD_ID, "storage_blocks/potato_from_potato_block"));
@@ -115,11 +98,6 @@ public class StorageBlocks extends MesonModule {
         if (!beetroot) {
             recipes.add(new Identifier(Covalent.MOD_ID, "storage_blocks/beetroot_block_from_beetroot"));
             recipes.add(new Identifier(Covalent.MOD_ID, "storage_blocks/beetroot_from_beetroot_block"));
-        }
-
-        if (!cyanDye) {
-            recipes.add(new Identifier(Covalent.MOD_ID, "storage_blocks/cyan_dye_block_from_cyan_dye"));
-            recipes.add(new Identifier(Covalent.MOD_ID, "storage_blocks/cyan_dye_from_cyan_dye_block"));
         }
 
         if (!carrot) {

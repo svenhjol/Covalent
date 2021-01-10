c85ac716a6963f3c0833fb0720ce611808c6b906
Coranthes <josa23oreo@gmail.com>
2021-01-10 02:03:22 -0500

    Terrestria compat

--- src/main/java/svenhjol/covalent/coranthemum/CoranthemumMaterial.java
+++ src/main/java/svenhjol/covalent/coranthemum/CoranthemumMaterial.java
@@ -3,7 +3,8 @@ package svenhjol.covalent.coranthemum;
 import svenhjol.charm.base.enums.IVariantMaterial;
 
 public enum CoranthemumMaterial implements IVariantMaterial {
-    PRISMARINE
+    PRISMARINE,
+    PURPUR
 
 }
 
--- src/main/java/svenhjol/covalent/terrestria/TerrestriaMaterial.java
+++ src/main/java/svenhjol/covalent/terrestria/TerrestriaMaterial.java
@@ -3,5 +3,13 @@ package svenhjol.covalent.terrestria;
 import svenhjol.charm.base.enums.IVariantMaterial;
 
 public enum TerrestriaMaterial implements IVariantMaterial {
-    JAPANESE_MAPLE
+    JAPANESE_MAPLE,
+    YUCCA_PALM,
+    WILLOW,
+    SAKURA,
+    RUBBER,
+    REDWOOD,
+    RAINBOW_EUCALYPTUS,
+    HEMLOCK,
+    CYPRESS
 }

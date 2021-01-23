97f66d6eaaffcf9bccb4f0b0698fd250cc458268
Coranthes <josa23oreo@gmail.com>
2021-01-22 21:09:12 -0500

    Better end integration stuff-mostly

--- src/main/java/svenhjol/covalent/CovalentIntegration.java
+++ src/main/java/svenhjol/covalent/CovalentIntegration.java
@@ -2,11 +2,8 @@ package svenhjol.covalent;
 
 import svenhjol.charm.base.enums.IVariantMaterial;
 import svenhjol.charm.base.helper.ModHelper;
-import svenhjol.covalent.integration.Coranthemum;
+import svenhjol.covalent.integration.*;
 import svenhjol.covalent.integration.Covalent;
-import svenhjol.covalent.integration.Terrestria;
-import svenhjol.covalent.integration.Traverse;
-import svenhjol.covalent.integration.WildExplorer;
 
 import java.util.*;
 
@@ -19,6 +16,7 @@ public class CovalentIntegration {
         MODS.put(Terrestria.MOD_ID, Terrestria.Materials.values());
         MODS.put(Traverse.MOD_ID, Traverse.Materials.values());
         MODS.put(WildExplorer.MOD_ID, WildExplorer.Materials.values());
+        MODS.put(BetterEnd.MOD_ID, BetterEnd.Materials.values());
     }
 
     public static List<IVariantMaterial> getMaterialsToRemove() {
---
+++ src/main/java/svenhjol/covalent/integration/BetterEnd.java
@@ -0,0 +1,20 @@
+package svenhjol.covalent.integration;
+
+import svenhjol.charm.base.enums.IVariantMaterial;
+
+public class BetterEnd {
+    public static final String MOD_ID = "better-end";
+
+    public enum Materials implements IVariantMaterial {
+        MOSSY_GLOWSHROOM,
+        TENANEA,
+        END_LOTUS,
+        LACUGROVE,
+        HELIX_TREE,
+        DRAGON_TREE,
+        PYTHADENDRON,
+        UMBRELLA_TREE
+    }
+}
+
+
